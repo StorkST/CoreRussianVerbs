1f851995ed66f71ad1e24584c08ff5a1a3aa0850
StorkST <vchd@pm.me>
2020-03-21 20:48:06 -0400

    MUIDatables template ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import MUIDataTable from "mui-datatables";
+import Papa from "papaparse";
 
+class App extends React.Component {
+  render() {
+    const columns = ["Name", "Title", "Location", "Age", "Salary"];
+		//For future use
+    const headers = columns.map((value, key) => {
+			if (key <= 3) {
+				return {
+					name: value,
+					options: {
+						display: true
+					}
+				};
+			} else
+				return {
+					name: value,
+					options: {
+						display: false
+					}
+				};
+		});
 
-const rootEl = document.getElementById('root');
+    const data = [
+      ["Gabby George", "Business Analyst", "Minneapolis", 30, "$100,000"],
+      ["Aiden Lloyd", "Business Consultant", "Dallas", 55, "$200,000"],
+      ["Jaden Collins", "Attorney", "Santa Ana", 27, "$500,000"],
+      ["Franky Rees", "Business Analyst", "St. Petersburg", 22, "$50,000"],
+     ["Aaren Rose", "Business Consultant", "Toledo", 28, "$75,000"]
+    ];
+    
+    const options = {
+      filterType: "dropdown",
+      responsive: "scroll"
+    };
 
-
-const App = () => {
     return (
-        <div>
-            <h1 style={{ 'textAlign': 'center'}}> Hello World :) </h1>
-        </div>
-        );
-};
-
-const renderApp = () => {
-    ReactDOM.render(
-        <App />, rootEl
+      <MUIDataTable
+        title={"ACME Employee list"}
+        data={data}
+        columns={columns}
+        options={options}
+      />
     );
+  }
 }
 
-renderApp();
-
-
-// This checks for local changes and automatically refreshes the browser (hot-reloading)
-if (module.hot) {
-    module.hot.accept('./components/App.jsx', () => renderApp());
-}
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/LoadCsv.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Papa from 'papaparse';
+
+class LoadCsv extends React.Component {
+    constructor() {
+        this.state = {
+            data: []
+        };
+
+        this.getData = this.getData.bind(this);
+    }
+
+    componentWillMount() {
+        this.getCsvData();
+    }
+
+    fetchCsv() {
+        return fetch('export-nostars.csv').then(function (response) {
+            let reader = response.body.getReader();
+            let decoder = new TextDecoder('utf-8');
+
+            return reader.read().then(function (result) {
+                //return decoder.decode(result.value);
+                return "ta,to,ti";
+            });
+        });
+    }
+
+    getData(result) {
+        this.setState({data: result.data});
+    }
+
+    //async getCsvData() {
+    getCsvData() {
+        //let csvData = await this.fetchCsv();
+        let csvData = this.fetchCsv();
+
+        Papa.parse(csvData, {
+            complete: this.getData
+        });
+    }
+
+    render() {
+        return (
+          <section className="LoadCsv">
+              <p>Voila des donnees: {this.state.data}</p>
+          </section>
+        );
+    }
+}
+
+//export default withRouter(DataController);
+ReactDOM.render(<LoadCsv />, document.getElementById("root"));
