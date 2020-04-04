18ffae57eb453aa2727bad2c718a9e070f809cf9
Pavel Voronin <p.voronin@focuscura.nl>
2020-04-04 17:20:45 +0200

    Load and show csv data.

---
+++ src/LanguageTable.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import MUIDataTable from "mui-datatables";
+import { fetchCsv } from "./csvUtils";
+
+const options = {
+  filterType: "dropdown",
+  responsive: "scroll",
+};
+
+const initialState = {
+  columns: [],
+  data: [],
+};
+
+export default function LanguageTable(props) {
+  const [state, updateState] = useState(initialState);
+
+  useEffect(() => {
+    fetchCsv({
+      url: "./data/export-nostars.csv",
+      encoding: "utf-8",
+      omitHeader: false,
+    }).then((data) => {
+      // data is array of arrays where first array contains column headers
+      // splice removes first array from data and returns headers array wrapped with another array
+      // that's why we have [0] at the end
+      const columns = data.splice(0, 1)[0];
+      updateState({ columns, data });
+    });
+  }, []);
+
+  // until we fetched and parsed all the data 
+  // we show nothing
+  if (state === initialState) {
+    return null;
+  }
+
+  return <MUIDataTable data={state.data} columns={state.columns} options={options} title="Verbs" />;
+}
